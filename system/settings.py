4abce0b4703f785017536a91b1ae90f3046ae6eb
Sérgio Rodrigues de Souza <sergiosrs1000@gmail.com>
2021-11-24 01:13:31 -0300

    lib django-on-heroku

--- system/settings.py
+++ system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_on_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -159,4 +159,4 @@ LOGIN_REDIRECT_URL = '/'
 #LOGOUT_REDIRECT_URL = '/admin/login/?next=/admin/'
 LOGOUT_REDIRECT_URL = '/'
 
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
